aa21d1ebc1f84f72ca640b5c438be35f31a930f5
Vladimir Solo <freeman.wushu@mail.ru>
2026-05-04 18:48:20 +0300

    feat: add error boundary and error btn

---
+++ src/components/ErrorButton/ErrorButton.tsx
@@ -0,0 +1,36 @@
+import { Component, ReactNode } from 'react';
+
+interface ErrorButtonState {
+  shouldThrow: boolean;
+}
+
+class ErrorButton extends Component<{}, ErrorButtonState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { shouldThrow: false };
+  }
+
+  handleClick = (): void => {
+    this.setState({ shouldThrow: true });
+  };
+
+  render(): ReactNode {
+    const { shouldThrow } = this.state;
+
+    if (shouldThrow) {
+      throw new Error('Test error triggered by user');
+    }
+
+    return (
+      <button
+        className="error-trigger-btn"
+        type="button"
+        onClick={this.handleClick}
+      >
+        Throw Error
+      </button>
+    );
+  }
+}
+
+export default ErrorButton;
