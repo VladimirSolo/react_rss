7649d1fa29cf8d77ed6e7cdc171844d02877ac6c
Vladimir Solo <freeman.wushu@mail.ru>
2026-05-18 19:28:41 +0300

    chore: install react-router-dom and extend Character type

--- src/types.ts
+++ src/types.ts
@@ -3,14 +3,21 @@ export interface Character {
   name: string;
   status: string;
   species: string;
+  type: string;
+  gender: string;
+  origin: { name: string; url: string };
+  location: { name: string; url: string };
+  image: string;
+}
+
+export interface ApiInfo {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
 }
 
 export interface ApiResponse {
-  info: {
-    count: number;
-    pages: number;
-    next: string | null;
-    prev: string | null;
-  };
+  info: ApiInfo;
   results: Character[];
 }
