68817756df49063c33f1348fbc85bb305ea3de0d
Vladimir Solo <freeman.wushu@mail.ru>
2026-05-25 20:26:58 +0300

    feat: integrate theme provider and flyout in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeProvider';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 import ErrorButton from './components/ErrorButton/ErrorButton';
 import Navigation from './components/Navigation/Navigation';
 import DetailPanel from './components/DetailPanel/DetailPanel';
+import Flyout from './components/Flyout/Flyout';
 import MainPage from './pages/MainPage/MainPage';
 import AboutPage from './pages/AboutPage/AboutPage';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
@@ -10,21 +12,24 @@ import './App.css';
 
 function App(): JSX.Element {
   return (
-    <div className="app">
-      <ErrorBoundary>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<MainPage />}>
-            <Route path="details/:id" element={<DetailPanel />} />
-          </Route>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-        <div className="footer-area">
-          <ErrorButton />
-        </div>
-      </ErrorBoundary>
-    </div>
+    <ThemeProvider>
+      <div className="app">
+        <ErrorBoundary>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<MainPage />}>
+              <Route path="details/:id" element={<DetailPanel />} />
+            </Route>
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <div className="footer-area">
+            <ErrorButton />
+          </div>
+        </ErrorBoundary>
+        <Flyout />
+      </div>
+    </ThemeProvider>
   );
 }
 
